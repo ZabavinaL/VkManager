d2de8193ba4d0618fc97c8e525495905cabb3aef
Lyubov Zabavina <zabavinal@yandex.ru>
2021-01-30 13:40:45 +0300

    NewSettingPost added, v2

---
+++ src/main/java/ru/netology/domain/NewSettingPost.java
@@ -0,0 +1,6 @@
+package ru.netology.domain;
+
+public class NewSettingPost {
+
+    private boolean hiddenPost; // отметка о том, скрыта ли запись для данного пользователя (если пользователю меньше 18 лет)
+}
