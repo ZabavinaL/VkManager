b15b5529ec59e449ec522a0e64a4e0ca6f3d0092
Lyubov Zabavina <zabavinal@yandex.ru>
2021-01-30 14:20:08 +0300

    PostManager class added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] wallSearch(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        // add logic
+        return null;
+    }
+
+    public void wallDelete(int ownerId, int postId) {
+        // add logic
+    }
+}
